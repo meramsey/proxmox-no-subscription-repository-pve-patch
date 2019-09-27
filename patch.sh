e2506c50ef205b63a302b9b1aa93ebef31264484
hellofaduck <49034105+hellofaduck@users.noreply.github.com>
2019-09-27 21:00:15 +0300

    Update patch.sh
    add directory change before download

--- patch.sh
+++ patch.sh
@@ -5,7 +5,7 @@ echo "- patch `pveversion`..."
 echo "- download and copy files..."
 cd /usr/share/pve-patch/images/
 wget -nc https://github.com/hellofaduck/pve-patch/raw/master/images/{favicon.ico,logo-128.png,proxmox_logo.png}
-/usr/share/pve-patch/scripts/
+cd /usr/share/pve-patch/scripts/
 wget -N https://raw.githubusercontent.com/hellofaduck/pve-patch/master/scripts/{90pvepatch,apply.sh}
 chmod -R a+x /usr/share/pve-patch/scripts
 cp -f /usr/share/pve-patch/scripts/90pvepatch /etc/apt/apt.conf.d/90pvepatch
