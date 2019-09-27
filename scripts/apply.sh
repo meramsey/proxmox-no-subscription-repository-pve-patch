568902b07d54d77382c0d6f237a5a7d6e3275141
hellofaduck <49034105+hellofaduck@users.noreply.github.com>
2019-09-27 20:35:49 +0300

    Update apply.sh
    change linux version to stretch

--- scripts/apply.sh
+++ scripts/apply.sh
@@ -2,7 +2,7 @@
 
 ENTERPRISE_REPO_LIST="/etc/apt/sources.list.d/pve-enterprise.list"
 FREE_REPO_LIST="/etc/apt/sources.list.d/pve.list"
-FREE_REPO_LINE="deb http://download.proxmox.com/debian/pve `lsb_release -cs` pve-no-subscription"
+FREE_REPO_LINE="deb http://download.proxmox.com/debian/pve stretch pve-no-subscription"
 
 function pve_patch() {
   echo "- apply patch..."
