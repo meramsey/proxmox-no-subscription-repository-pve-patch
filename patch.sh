638a7813a8cf121d780934199dea338b745d9ada
hellofaduck <49034105+hellofaduck@users.noreply.github.com>
2019-09-27 20:58:50 +0300

    Update patch.sh

--- patch.sh
+++ patch.sh
@@ -3,8 +3,10 @@
 mkdir -p /usr/share/pve-patch/{images,scripts}
 echo "- patch `pveversion`..."
 echo "- download and copy files..."
-wget -nc /usr/share/pve-patch/images/ https://github.com/hellofaduck/pve-patch/raw/master/images/{favicon.ico,logo-128.png,proxmox_logo.png}
-wget /usr/share/pve-patch/scripts/ https://raw.githubusercontent.com/hellofaduck/pve-patch/master/scripts/{90pvepatch,apply.sh}
+cd /usr/share/pve-patch/images/
+wget -nc https://github.com/hellofaduck/pve-patch/raw/master/images/{favicon.ico,logo-128.png,proxmox_logo.png}
+/usr/share/pve-patch/scripts/
+wget -N https://raw.githubusercontent.com/hellofaduck/pve-patch/master/scripts/{90pvepatch,apply.sh}
 chmod -R a+x /usr/share/pve-patch/scripts
 cp -f /usr/share/pve-patch/scripts/90pvepatch /etc/apt/apt.conf.d/90pvepatch
 /usr/share/pve-patch/scripts/apply.sh
