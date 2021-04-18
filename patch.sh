5898d590e0b622467603b50c361cb9e78a574b58
Michael Ramsey <mike@hackerdise.me>
2021-04-18 19:50:58 +0000

    Update patch.sh

--- patch.sh
+++ patch.sh
@@ -3,11 +3,12 @@
 mkdir -p /usr/share/pve-patch/{images,scripts}
 echo "- patch `pveversion`..."
 echo "- download and copy files..."
-cd /usr/share/pve-patch/scripts/
-wget -O https://gitlab.com/mikeramsey/proxmox-no-subscription-repository-pve-patch/-/raw/master/scripts/{90pvepatch,apply.sh}
-cd /usr/share/pve-patch/images/
-wget -nc https://gitlab.com/mikeramsey/proxmox-no-subscription-repository-pve-patch/-/raw/master/images/{favicon.ico,logo-128.png,proxmox_logo.png}
+# We want to overwrite so updates work seamlessly if script updated.
+wget -O /usr/share/pve-patch/scripts/90pvepatch https://gitlab.com/mikeramsey/proxmox-no-subscription-repository-pve-patch/-/raw/master/scripts/90pvepatch
+wget -O /usr/share/pve-patch/scripts/apply.sh https://gitlab.com/mikeramsey/proxmox-no-subscription-repository-pve-patch/-/raw/master/scripts/apply.sh
+
+cd /usr/share/pve-patch/images/ && wget -nc https://gitlab.com/mikeramsey/proxmox-no-subscription-repository-pve-patch/-/raw/master/images/{favicon.ico,logo-128.png,proxmox_logo.png}
 chmod -R a+x /usr/share/pve-patch/scripts
-cp -f /usr/share/pve-patch/scripts/90pvepatch /etc/apt/apt.conf.d/90pvepatch
+/cp -f /usr/share/pve-patch/scripts/90pvepatch /etc/apt/apt.conf.d/90pvepatch
 /usr/share/pve-patch/scripts/apply.sh
 echo "- done!"
