b9e65ed175020adb0bd476e8c7c32f082d3f7493
Michael Ramsey <mike@hackerdise.me>
2021-04-18 19:38:56 +0000

    Update patch.sh

--- patch.sh
+++ patch.sh
@@ -4,7 +4,7 @@ mkdir -p /usr/share/pve-patch/{images,scripts}
 echo "- patch `pveversion`..."
 echo "- download and copy files..."
 cd /usr/share/pve-patch/scripts/
-wget -N https://gitlab.com/mikeramsey/proxmox-no-subscription-repository-pve-patch/-/raw/master/scripts/{90pvepatch,apply.sh}
+wget -O https://gitlab.com/mikeramsey/proxmox-no-subscription-repository-pve-patch/-/raw/master/scripts/{90pvepatch,apply.sh}
 cd /usr/share/pve-patch/images/
 wget -nc https://gitlab.com/mikeramsey/proxmox-no-subscription-repository-pve-patch/-/raw/master/images/{favicon.ico,logo-128.png,proxmox_logo.png}
 chmod -R a+x /usr/share/pve-patch/scripts
