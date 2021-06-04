abfe0b6dfa50d229b4be89f4e01bc57bffb3e8e4
Michael Ramsey <mike@hackerdise.me>
2021-06-04 19:52:34 +0000

    Update patch.sh

--- patch.sh
+++ patch.sh
@@ -7,8 +7,11 @@ echo "- download and copy files..."
 wget -O /usr/share/pve-patch/scripts/90pvepatch https://gitlab.com/mikeramsey/proxmox-no-subscription-repository-pve-patch/-/raw/master/scripts/90pvepatch
 wget -O /usr/share/pve-patch/scripts/apply.sh https://gitlab.com/mikeramsey/proxmox-no-subscription-repository-pve-patch/-/raw/master/scripts/apply.sh
 
-cd /usr/share/pve-patch/images/ && wget -nc https://gitlab.com/mikeramsey/proxmox-no-subscription-repository-pve-patch/-/raw/master/images/{favicon.ico,logo-128.png,proxmox_logo.png}
+# cd /usr/share/pve-patch/images/ && wget -nc https://gitlab.com/mikeramsey/proxmox-no-subscription-repository-pve-patch/-/raw/master/images/{favicon.ico,logo-128.png,proxmox_logo.png}
 chmod -R a+x /usr/share/pve-patch/scripts
-/cp -f /usr/share/pve-patch/scripts/90pvepatch /etc/apt/apt.conf.d/90pvepatch
-/usr/share/pve-patch/scripts/apply.sh
+cp -f /usr/share/pve-patch/scripts/90pvepatch /etc/apt/apt.conf.d/90pvepatch
+bash /usr/share/pve-patch/scripts/apply.sh
+
+# pvedarkdiscord theme: courtesy of https://github.com/Weilbyte/PVEDiscordDark
+bash <(curl -s https://raw.githubusercontent.com/Weilbyte/PVEDiscordDark/master/PVEDiscordDark.sh ) install
 echo "- done!"
